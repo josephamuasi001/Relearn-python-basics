def56bc61c79a8281689078a2f7b152b01e2615f
Joseph Amuasi <jamuasi001@st.ug.edu.gh>
2026-04-09 16:50:12 +0000

    Mini practice Basics

---
+++ minipractice.py
@@ -0,0 +1,12 @@
+# Write a program that:
+
+# Asks for your name
+# Asks for your age
+# Prints:
+# My name is ___ and I am ___ years old
+
+
+name = input("Enter your name here: ")
+age = int(input("Enter your age here: "))
+
+print(f"My name is {name} and I am {age} years old")
